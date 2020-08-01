694f9e92cf54ce18212c0ea90c9d01367968cf09
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-01 22:45:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
-  counter
- array.each do |numbers|
-   new_array = []
-  return numbers * numbers
-end
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
